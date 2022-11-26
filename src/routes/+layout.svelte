e02a60bb440213cd33c9191111d24dc6994f2e8d
steelWinds <kirillsurov0@gmail.com>
2022-11-26 10:04:07 +0900

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
 	import { SvelteToast } from '@zerodevx/svelte-toast';
 	import config from '@/configs/svelte-toasts/svelte-toast-config';
 	import pointsStore from '@/stores/points';
 
 	import '../app.css';
 
-	pointsStore.updatePoints();
+  onMount(() => {
+    pointsStore.updatePoints();
+  })
 </script>
 
 <main data-sveltekit-prefetch>
